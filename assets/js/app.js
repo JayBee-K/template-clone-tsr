a842530bfc00bbb3c59c85675bbb61301a14afc8
Jaybee-k <79182784+JayBee-K@users.noreply.github.com>
2022-08-27 20:45:26 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,27 @@
 ;(function ($) {
-	'use strict';
+    'use strict';
 
-	let initFromModule1 = function () {
-		let elmWrapper = $('#from-m1');
-		$('#from-m1').on('click', '.addRow', function () {
-			let rowRender = `<div class="row-item row custom-row">
-							<div class="col-lg-3 col-sm-12 col-12">
-								<select name="" class="form-control" id="">
-									<option value="">Viettel(85,88)</option>
-									<option value="">Vina(88,91)</option>
-									<option value="">Mobifone(77,80)</option>
-									<option value="">Vietnammobi(85,88)</option>
-									<option value="">Zing</option>
-									<option value="">GATE</option>
-								</select>
-							</div>
+    let initFromModule1 = function () {
+        let elmWrapper = $('#from-m1');
+        $('#from-m1').on('click', '.addRow', function () {
+            let theCaoSelect = $(this).closest('.row-item').find('#theCao'),
+                theCaoIndexSelected = theCaoSelect.find('option:selected').attr('data-index'),
+                theCaoRender = $(this).closest('.row-item').find('#theCao').clone();
+
+            theCaoRender.find('option').attr('selected', false);
+            theCaoRender.find('option[data-index=' + theCaoIndexSelected + ']').attr('selected', true);
+
+            let menhGiaSelect = $(this).closest('.row-item').find('#menhGia'),
+                menhGiaIndexSelected = menhGiaSelect.find('option:selected').attr('data-index'),
+                menhGiaRender = $(this).closest('.row-item').find('#menhGia').clone();
+
+            menhGiaRender.find('option').attr('selected', false);
+            menhGiaRender.find('option[data-index=' + menhGiaIndexSelected + ']').attr('selected', true);
+
+            let rowRender = `<div class="row-item row row5">
+                            <div class="col-lg-3 col-sm-12 col-12">
+							    ${theCaoRender[0].innerHTML}
+							 </div>
 							<div class="col-lg-3 col-sm-6 col-12">
 								<input type="text" class="form-control" placeholder="Mã thẻ">
 							</div>
@@ -22,18 +29,7 @@
 								<input type="text" class="form-control" placeholder="Mã seri">
 							</div>
 							<div class="col-lg-2 col-sm-10 col-9">
-								<select name="" class="form-control" id="">
-									<option value="">-- Mệnh giá --</option>
-									<option value="">10,000 đ</option>
-									<option value="">20,000 đ</option>
-									<option value="">30,000 đ</option>
-									<option value="">50,000 đ</option>
-									<option value="">100,000 đ</option>
-									<option value="">200,000 đ</option>
-									<option value="">300,000 đ</option>
-									<option value="">500,000 đ</option>
-									<option value="">1,000,000 đ</option>
-								</select>
+							    ${menhGiaRender[0].innerHTML}
 							</div>
 							<div class="col-lg-1 col-sm-2 col-3 text-right">
 								<button type="button" class="btn btn-small btn-danger deleteRow">
@@ -41,13 +37,21 @@
 								</button>
 							</div>
 						</div>`;
-			elmWrapper.append(rowRender);
-		}).on('click', '.deleteRow', function () {
-			$(this).closest('.row-item').remove();
-		});
-	}
+            elmWrapper.append(rowRender);
+        }).on('click', '.deleteRow', function () {
+            $(this).closest('.row-item').remove();
+        });
+    }
+    let initCardCloneTab = function () {
+        $('.card-cloneTab[data-toggle=tab]').on('shown.bs.tab', function () {
+            $('.card-cloneTab[data-toggle=tab]').removeClass('active');
+            $(this).addClass('active');
+            $(this).parent().addClass('active');
+        });
+    }
 
-	$(function () {
-		initFromModule1();
-	});
+    $(function () {
+        initFromModule1();
+        initCardCloneTab();
+    });
 })(jQuery);
